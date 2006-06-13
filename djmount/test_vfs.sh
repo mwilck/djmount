7bfe67bd2ea03ea203901e85b79fb99cb460f510
r3mi <r3mi@d2b06c04-d219-0410-9cba-8d529fbc48d6>
2006-06-13 19:38:00 +0000

    add copyright header + add test for file content
    git-svn-id: https://svn.code.sf.net/p/djmount/code/trunk@191 d2b06c04-d219-0410-9cba-8d529fbc48d6

--- djmount/test_vfs.sh
+++ djmount/test_vfs.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+# $Id$
+# 
+# Test VFS object.
+# This file is part of djmount.
+# 
+# (C) Copyright 2005 Rémi Turboult <r3mi@users.sourceforge.net>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
 
 
 
@@ -84,6 +105,9 @@ diff - <(find . -print) <<-EOF || exit 1
 	./.debug/talloc_report
 	./.debug/talloc_report_full
 EOF
+
+
+echo -n "essais" | diff - ./atest/test/a2/b1/f1 || exit 1
 
 
 diff - <(/bin/ls -lR) <<-EOF || exit 1 
