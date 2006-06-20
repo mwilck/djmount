35255473941f0104a9cf5dc9e503d35663782d51
r3mi <r3mi@d2b06c04-d219-0410-9cba-8d529fbc48d6>
2006-06-20 20:54:55 +0000

    make sure to run test using C locale
    git-svn-id: https://svn.code.sf.net/p/djmount/code/trunk@199 d2b06c04-d219-0410-9cba-8d529fbc48d6

--- djmount/test_vfs.sh
+++ djmount/test_vfs.sh
@@ -113,262 +113,266 @@ EOF
 
 echo -n "essais" | diff - ./atest/test/a2/b1/f1 || exit 1
 
+# Make sure "ls -l" display in standard C locale e.g. month names
+export LANG=C
+export LC_ALL=C
+
 
 diff -u - <(/bin/ls -lR) <<-EOF || exit 1 
 .:
 total 2
-dr-xr-xr-x  3 root root 512 jan  1  2000 atest
-lr--r--r--  1 root root  11 jan  1  2000 broken_link -> broken/link
-dr-xr-xr-x  5 root root 512 jan  1  2000 test
--r--r--r--  1 root root   0 jan  1  2000 void_file
-dr-xr-xr-x  3 root root 512 jan  1  2000 zetest
+dr-xr-xr-x  3 root root 512 Jan  1  2000 atest
+lr--r--r--  1 root root  11 Jan  1  2000 broken_link -> broken/link
+dr-xr-xr-x  5 root root 512 Jan  1  2000 test
+-r--r--r--  1 root root   0 Jan  1  2000 void_file
+dr-xr-xr-x  3 root root 512 Jan  1  2000 zetest
 
 ./atest:
 total 1
-lr--r--r--  1 root root   4 jan  1  2000 link_to_test -> test
-dr-xr-xr-x  5 root root 512 jan  1  2000 test
+lr--r--r--  1 root root   4 Jan  1  2000 link_to_test -> test
+dr-xr-xr-x  5 root root 512 Jan  1  2000 test
 
 ./atest/test:
 total 2
-dr-xr-xr-x  2 root root 512 jan  1  2000 a1
-dr-xr-xr-x  4 root root 512 jan  1  2000 a2
-dr-xr-xr-x  9 root root 512 jan  1  2000 a3
+dr-xr-xr-x  2 root root 512 Jan  1  2000 a1
+dr-xr-xr-x  4 root root 512 Jan  1  2000 a2
+dr-xr-xr-x  9 root root 512 Jan  1  2000 a3
 
 ./atest/test/a1:
 total 0
 
 ./atest/test/a2:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 b1
-dr-xr-xr-x  3 root root 512 jan  1  2000 b2
+dr-xr-xr-x  2 root root 512 Jan  1  2000 b1
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b2
 
 ./atest/test/a2/b1:
 total 1
--r--r--r--  1 root root 6 jan  1  2000 f1
+-r--r--r--  1 root root 6 Jan  1  2000 f1
 
 ./atest/test/a2/b2:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 c1
+dr-xr-xr-x  2 root root 512 Jan  1  2000 c1
 
 ./atest/test/a2/b2/c1:
 total 0
 
 ./atest/test/a3:
 total 4
-dr-xr-xr-x  2 root root 512 jan  1  2000 b3
-dr-xr-xr-x  3 root root 512 jan  1  2000 b4
-dr-xr-xr-x  3 root root 512 jan  1  2000 b5
-dr-xr-xr-x  3 root root 512 jan  1  2000 b6
-dr-xr-xr-x  3 root root 512 jan  1  2000 b7
-dr-xr-xr-x  3 root root 512 jan  1  2000 b8
-dr-xr-xr-x  3 root root 512 jan  1  2000 b9
+dr-xr-xr-x  2 root root 512 Jan  1  2000 b3
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b4
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b5
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b6
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b7
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b8
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b9
 
 ./atest/test/a3/b3:
 total 0
 
 ./atest/test/a3/b4:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./atest/test/a3/b4/toto:
 total 0
 
 ./atest/test/a3/b5:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./atest/test/a3/b5/toto:
 total 0
 
 ./atest/test/a3/b6:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./atest/test/a3/b6/toto:
 total 0
 
 ./atest/test/a3/b7:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./atest/test/a3/b7/toto:
 total 0
 
 ./atest/test/a3/b8:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./atest/test/a3/b8/toto:
 total 0
 
 ./atest/test/a3/b9:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./atest/test/a3/b9/toto:
 total 0
 
 ./test:
 total 2
-dr-xr-xr-x  2 root root 512 jan  1  2000 a1
-dr-xr-xr-x  4 root root 512 jan  1  2000 a2
-dr-xr-xr-x  9 root root 512 jan  1  2000 a3
+dr-xr-xr-x  2 root root 512 Jan  1  2000 a1
+dr-xr-xr-x  4 root root 512 Jan  1  2000 a2
+dr-xr-xr-x  9 root root 512 Jan  1  2000 a3
 
 ./test/a1:
 total 0
 
 ./test/a2:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 b1
-dr-xr-xr-x  3 root root 512 jan  1  2000 b2
+dr-xr-xr-x  2 root root 512 Jan  1  2000 b1
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b2
 
 ./test/a2/b1:
 total 1
--r--r--r--  1 root root 6 jan  1  2000 f1
+-r--r--r--  1 root root 6 Jan  1  2000 f1
 
 ./test/a2/b2:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 c1
+dr-xr-xr-x  2 root root 512 Jan  1  2000 c1
 
 ./test/a2/b2/c1:
 total 0
 
 ./test/a3:
 total 4
-dr-xr-xr-x  2 root root 512 jan  1  2000 b3
-dr-xr-xr-x  3 root root 512 jan  1  2000 b4
-dr-xr-xr-x  3 root root 512 jan  1  2000 b5
-dr-xr-xr-x  3 root root 512 jan  1  2000 b6
-dr-xr-xr-x  3 root root 512 jan  1  2000 b7
-dr-xr-xr-x  3 root root 512 jan  1  2000 b8
-dr-xr-xr-x  3 root root 512 jan  1  2000 b9
+dr-xr-xr-x  2 root root 512 Jan  1  2000 b3
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b4
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b5
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b6
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b7
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b8
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b9
 
 ./test/a3/b3:
 total 0
 
 ./test/a3/b4:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./test/a3/b4/toto:
 total 0
 
 ./test/a3/b5:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./test/a3/b5/toto:
 total 0
 
 ./test/a3/b6:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./test/a3/b6/toto:
 total 0
 
 ./test/a3/b7:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./test/a3/b7/toto:
 total 0
 
 ./test/a3/b8:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./test/a3/b8/toto:
 total 0
 
 ./test/a3/b9:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./test/a3/b9/toto:
 total 0
 
 ./zetest:
 total 1
-lr--r--r--  1 root root   7 jan  1  2000 link_to_test -> ../test
-dr-xr-xr-x  5 root root 512 jan  1  2000 test
+lr--r--r--  1 root root   7 Jan  1  2000 link_to_test -> ../test
+dr-xr-xr-x  5 root root 512 Jan  1  2000 test
 
 ./zetest/test:
 total 2
-dr-xr-xr-x  2 root root 512 jan  1  2000 a1
-dr-xr-xr-x  4 root root 512 jan  1  2000 a2
-dr-xr-xr-x  9 root root 512 jan  1  2000 a3
+dr-xr-xr-x  2 root root 512 Jan  1  2000 a1
+dr-xr-xr-x  4 root root 512 Jan  1  2000 a2
+dr-xr-xr-x  9 root root 512 Jan  1  2000 a3
 
 ./zetest/test/a1:
 total 0
 
 ./zetest/test/a2:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 b1
-dr-xr-xr-x  3 root root 512 jan  1  2000 b2
+dr-xr-xr-x  2 root root 512 Jan  1  2000 b1
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b2
 
 ./zetest/test/a2/b1:
 total 1
--r--r--r--  1 root root 6 jan  1  2000 f1
+-r--r--r--  1 root root 6 Jan  1  2000 f1
 
 ./zetest/test/a2/b2:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 c1
+dr-xr-xr-x  2 root root 512 Jan  1  2000 c1
 
 ./zetest/test/a2/b2/c1:
 total 0
 
 ./zetest/test/a3:
 total 4
-dr-xr-xr-x  2 root root 512 jan  1  2000 b3
-dr-xr-xr-x  3 root root 512 jan  1  2000 b4
-dr-xr-xr-x  3 root root 512 jan  1  2000 b5
-dr-xr-xr-x  3 root root 512 jan  1  2000 b6
-dr-xr-xr-x  3 root root 512 jan  1  2000 b7
-dr-xr-xr-x  3 root root 512 jan  1  2000 b8
-dr-xr-xr-x  3 root root 512 jan  1  2000 b9
+dr-xr-xr-x  2 root root 512 Jan  1  2000 b3
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b4
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b5
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b6
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b7
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b8
+dr-xr-xr-x  3 root root 512 Jan  1  2000 b9
 
 ./zetest/test/a3/b3:
 total 0
 
 ./zetest/test/a3/b4:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./zetest/test/a3/b4/toto:
 total 0
 
 ./zetest/test/a3/b5:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./zetest/test/a3/b5/toto:
 total 0
 
 ./zetest/test/a3/b6:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./zetest/test/a3/b6/toto:
 total 0
 
 ./zetest/test/a3/b7:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./zetest/test/a3/b7/toto:
 total 0
 
 ./zetest/test/a3/b8:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./zetest/test/a3/b8/toto:
 total 0
 
 ./zetest/test/a3/b9:
 total 1
-dr-xr-xr-x  2 root root 512 jan  1  2000 toto
+dr-xr-xr-x  2 root root 512 Jan  1  2000 toto
 
 ./zetest/test/a3/b9/toto:
 total 0
