e5cb4a99622313e8fc4f8a5d94706370f31a9efa
Yuval Tal <yuvalt@boxee.tv>
2011-07-04 18:08:59 +0300

    forgotten flag for configure

--- configure_canmore.sh
+++ configure_canmore.sh
@@ -4,4 +4,4 @@ export CXX=/opt/canmore/IntelCE/bin/i686-cm-linux-g++
 export CC=/opt/canmore/IntelCE/bin/i686-cm-linux-gcc
 export CPPFLAGS=-I/opt/canmore/local/include 
 export LDFLAGS="-L/opt/canmore/local/lib -liconv"
-./configure --host=i686-cm-linux
+./configure --host=i686-cm-linux --prefix=/opt/canmore/local
